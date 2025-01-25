f6647a0434eb92aca9f6fa8f3b9f034a2616a3b4
tle-dref <thomas.ledref@gmail.com>
2025-01-25 17:19:40 +0100

    fix serializer

--- djangoapp/serializers.py
+++ djangoapp/serializers.py
@@ -4,14 +4,14 @@ from .models import User, History
 class HistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = History
-        fields = ['id', 'user', 'guest_name', 'user_score', 'guest_score', 'played_at', 'profile_picture']
+        fields = ['id', 'user', 'guest_name', 'user_score', 'guest_score', 'played_at']
 
 class UserSerializer(serializers.ModelSerializer):
     histories = HistorySerializer(many=True, read_only=True)  # Relation avec History
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'histories']  # Ajout de histories
+        fields = ['id', 'username', 'email', 'password', 'histories', 'profile_picture']  # Ajout de histories
         extra_kwargs = {
             'password': {'write_only': True},  # Cache le mot de passe lors de la lecture
         }
