c59a4ee49cc7ecaa7bd85e2d0d946199f8002b00
Damien Lebranchu <dalebran@student.42lehavre.fr>
2025-01-27 20:05:07 +0100

    +: FriendItem.vue & FriendList.vue on chat folder

--- frontend/src/components/chat/FriendList.vue
+++ frontend/src/components/chat/FriendList.vue
@@ -61,7 +61,7 @@
 import { ref, computed, onMounted } from 'vue'
 import { useAuthStore } from '../stores/authStore'
 import { useFriendStore } from '../stores/friendStore'
-import AddFriendIcon from './icons/AddFriendIcon.vue'
+import AddFriendIcon from '../icons/AddFriendIcon.vue'
 import FriendItem from './FriendItem.vue'
 
 const authStore = useAuthStore()
