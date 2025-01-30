be764f1540670bcc81adedf4cdf83f1209e4983a
tle-dref <thomas.ledref@gmail.com>
2025-01-30 11:13:23 +0100

    ~: FIX STATUS DE MERDE

--- backend/djangoapp/views.py
+++ backend/djangoapp/views.py
@@ -13,11 +13,13 @@ from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from .models import UserStatus
 
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def online_users(request):
-    online_users = UserStatus.objects.filter(is_online=True).values_list('user_id', flat=True)
-    return Response(list(online_users), status=200)
+    online_users = UserStatus.objects.filter(is_online=True).values("user__id", "user__username")
+    return Response({"online_users": list(online_users)}, status=200)
+
 
 
 @api_view(['POST'])
