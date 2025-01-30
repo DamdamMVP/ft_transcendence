318496a292f4e5a4aa60f6a00920d81226b276d9
tle-dref <thomas.ledref@gmail.com>
2025-01-30 02:23:54 +0100

    ~: fix la merde de gamian

--- frontend/src/components/SignIn.vue
+++ frontend/src/components/SignIn.vue
@@ -14,7 +14,7 @@ const email = ref('')
 const password = ref('')
 const error = ref('')
 const isLoading = ref(false)
-
+const { signIn } = useAuth()
 const handleSignIn = async () => {
   if (!email.value || !password.value) {
     error.value = 'Veuillez remplir tous les champs'
