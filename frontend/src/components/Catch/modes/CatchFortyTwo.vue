f90917d4a1b4e202011a9c84ad45c2dcaa8ede90
Gaetan Bruscani <gbruscan@c2r2p5.42lehavre.fr>
2025-02-01 03:34:03 +0100

    jeux centre

--- frontend/src/components/Catch/modes/CatchFortyTwo.vue
+++ frontend/src/components/Catch/modes/CatchFortyTwo.vue
@@ -57,6 +57,15 @@ export default {
 .catch-game {
   width: 100%;
   height: 100vh;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background: var(--background-color);
 }
 
