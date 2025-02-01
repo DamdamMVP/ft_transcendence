8b643f28fbfb11bde3564d36902accbdce6c4d2e
Gaetan Bruscani <gbruscan@c2r2p5.42lehavre.fr>
2025-02-01 07:12:59 +0100

    fix de la barre

--- frontend/src/views/CatchView.vue
+++ frontend/src/views/CatchView.vue
@@ -41,12 +41,6 @@ export default {
 .catch-view {
   width: 100%;
   height: 100vh;
-  overflow: hidden;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
   display: flex;
   justify-content: center;
   align-items: center;
