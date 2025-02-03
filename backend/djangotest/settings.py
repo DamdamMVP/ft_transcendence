b6c0cb9b1b71c44ec3c4756d5f9f3cb9802d05ff
Damien Lebranchu <dalebran@student.42lehavre.fr>
2025-02-03 10:30:06 +0100

    +: last fix

--- backend/djangotest/settings.py
+++ backend/djangotest/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-vvw5a!owa!r6eqep12wpb1!k^bzx+ey=_z11-5n_$y6dfi$a!6')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
